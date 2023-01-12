6126069614be4a75459b7ad9b940c74e186fc303
Grigoryev Pavel <Green-ono@yandex.ru>
2023-01-12 23:00:24 +0300

    added exceptionHandling() for SecurityConfig

--- src/main/java/by/grigoryev/linkshortener/config/SecurityConfig.java
+++ src/main/java/by/grigoryev/linkshortener/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package by.grigoryev.linkshortener.config;
 
 import by.grigoryev.linkshortener.security.JwtAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -29,12 +35,26 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> unauthorized(response))
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    private static void unauthorized(HttpServletResponse response) throws IOException {
+        Map<String, Object> responseMap = new LinkedHashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(401);
+        responseMap.put("error", true);
+        responseMap.put("message", "Unauthorized");
+        response.setHeader("content-type", "application/json");
+        String responseMessage = mapper.writeValueAsString(responseMap);
+        response.getWriter().write(responseMessage);
     }
 
 }
--- src/main/java/by/grigoryev/linkshortener/security/JwtAuthenticationFilter.java
+++ src/main/java/by/grigoryev/linkshortener/security/JwtAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package by.grigoryev.linkshortener.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -30,24 +34,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities()
-                );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
             }
+            jwt = authHeader.substring(7);
+            userEmail = jwtService.extractUsername(jwt);
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        } catch (MalformedJwtException e) {
+            log.error("Wrong Jwt token");
+        } catch (IllegalArgumentException e) {
+            log.error("Unable to fetch JWT Token");
+        } catch (ExpiredJwtException e) {
+            log.error("JWT Token is expired");
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
         }
         filterChain.doFilter(request, response);
     }
